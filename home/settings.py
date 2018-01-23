fb15ae758b6333e4ec34593acd46154297715aea
Firexd2 <firedenis@yandex.ru>
2018-01-23 16:21:25 +0300

    add static_root

--- home/settings.py
+++ home/settings.py
@@ -125,3 +125,5 @@ STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 )
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
