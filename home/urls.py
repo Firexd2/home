2dacb10d65a00d472054e97cdd706cd4c5388bdb
Firexd2 <firedenis@yandex.ru>
2018-01-23 14:54:40 +0300

    finished dev templates

--- home/urls.py
+++ home/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from home import settings
-from page import views
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
